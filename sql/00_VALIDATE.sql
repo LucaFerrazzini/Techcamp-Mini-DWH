d7ef51279c2b849f7b4c0ab15430d9e5b3037c30
LFE <LFE@Lucas-MacBook-Pro.local>
2022-03-19 11:03:29 +0100

    SQL vaildator FAIL

--- sql/00_VALIDATE.sql
+++ sql/00_VALIDATE.sql
@@ -1,7 +1,12 @@
 CREATE OR ALTER PROCEDURE VALIDATE_SQL (@sql varchar(max)) AS
 BEGIN
+    BEGIN TRY
         SET @sql = 'SET PARSEONLY ON;' + REPLACE ( @sql , '''' , '''''' );
         EXEC(@sql);
+    END TRY
+    BEGIN CATCH
+        RAISERROR('##vso[task.logissue type=error]SQL-Script contains synrtax-errors!',0,1);
+    END CATCH;
 END; -- IsValidSQL
 
 EXEC VALIDATE_SQL @sql = '
