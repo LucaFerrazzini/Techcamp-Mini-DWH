5e1c40dab93c03f28b2fa2f8865018ddd9916a76
LFE <LFE@Lucas-MacBook-Pro.local>
2022-03-19 10:46:26 +0100

    SQL vaildator FAIL

--- sql/00_VALIDATE.sql
+++ sql/00_VALIDATE.sql
@@ -1,15 +1,7 @@
 CREATE OR ALTER PROCEDURE VALIDATE_SQL (@sql varchar(max)) AS
 BEGIN
-    BEGIN TRY
         SET @sql = 'SET PARSEONLY ON;' + REPLACE ( @sql , '''' , '''''' );
         EXEC(@sql);
-    END TRY
-    BEGIN CATCH
-        RETURN RAISERROR ('SQL deployment script does contain syntax error!', -- Message text.  
-               16, -- Severity.  
-               -1 -- State.  
-               ); 
-    END CATCH;
 END; -- IsValidSQL
 
 EXEC VALIDATE_SQL @sql = '
